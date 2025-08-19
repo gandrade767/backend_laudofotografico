1da000d1e726dcc29f32554029e83d575045771d
gandrade767 <gandrade767@gmail.com>
2025-08-19 10:49:53 -0400

    Laudo Router

---
+++ src/modules/laudos/laudo.routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { validate } = require('../../middlewares/validate');
+const { requireAuth } = require('../../middlewares/auth');
+const { requireRole } = require('../../middlewares/rbac');
+const { createLaudo, listLaudoQuerySchema, idParamSchema, createLaudoSchema } = require('./laudo.schema');
+const { createLaudo, listLaudos, getLaudo } = require('./laudo.controller');
+
+const router = express.Router();
+
+//apenas admin/usuario
+router.post('/', requireAuth, requireRole(['admin','usuario']),
+  validate({ body: createLaudoSchema }), createLaudo);
+
+router.get('/', requireAuth, requireRole(['admin','usuario']),
+  validate({ query: listLaudosQuerySchema }), listLaudos);
+
+router.get('/:id', requireAuth, requireRole(['admin','usuario']),
+  validate({ params: idParamSchema }), getLaudo);
+
+module.exports = router;
